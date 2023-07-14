e21cb640c069e33dbb4063f4fcb1a27b6c78af6a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-07-15 01:08:03 +0500

    complete exercise 21

--- 21exercise_conditional_tests/index.ts
+++ 21exercise_conditional_tests/index.ts
@@ -28,3 +28,33 @@ console.log(`Statement: If a string is empty? | Predict: True | Result: ${myName
 console.log(" ==================== \n")
 
 console.log(" ---------- Returns False ----------\n")
+
+console.log(numbers)
+console.log(`Statement: If an array is empty | Predict: False | Result: ${numbers.length == 0}`)
+
+console.log(" ==================== \n")
+
+let onlyNumbers = '2'
+console.log(onlyNumbers)
+console.log(`Type of onlyNumbers variable is ${typeof(onlyNumbers)}`)
+console.log(`Statement: If a number is an integer | Predict: False | Result: ${typeof(onlyNumbers) == 'number'}`)
+
+console.log(" ==================== \n")
+
+let numberInString = '2'
+let numberInInteger = 2
+console.log(`Value of numberInString var is ${numberInString} and type of var is ${typeof(numberInString)}`)
+console.log(`Value of numberInInteger var is ${numberInInteger} and type of var is ${typeof(numberInInteger)}`)
+
+console.log(`Statement: If both variables are of the same type | Predict: False | Result: ${typeof(numberInString) == typeof(numberInInteger)}`)
+
+console.log(" ==================== \n")
+
+let car = 'Honda Civic'
+console.log(car)
+console.log(`Statement: If a Car is Land Cruiser | Predict: False | Result: ${car == "Land Cruiser"}`)
+
+console.log(" ==================== \n")
+
+let typescriptFact = 'Typescript provides type safety'
+console.log(`Statement: If 'Typescript' does not provide 'Type Safety' | Predict: False | Result: ${!typescriptFact}`)

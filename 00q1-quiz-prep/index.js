3f7a003674b454866d38e8ac177bc73d368bd65a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-10-05 23:58:20 +0500

    some more notes from chapter 2

--- 00q1-quiz-prep/index.js
+++ 00q1-quiz-prep/index.js
@@ -43,13 +43,42 @@ function paintPainting(painter: Painter, painting: string) {
  *
  *
  */
-let fullname = "Abdullah";
 // Type Inferring
 // Typescript khud se hi type infer karleti hai because of being it smart enough. Typescript har us variable ki type infer kar sakti hai jiski starting value computed ho.
 // In this example, TypeScript knows that the ternary expression always results in a string
 // let bestAudio = Math.random() > 0.5 ? "Hamd" : "Simple Voice"
 // console.log(typeof(bestAudio)) // Inferred type: string
-fullname.length();
+// Type errors
+// let fullname = "Abdullah"
+// fullname.length() // Type error - Typescript be fullname variable ki type "string" infer ki thi and "length" property "number" hai and not function.
+// fullname.length // No Type error
+// Assignability
+// These are allowed because we are assigning string to the variable which type was inferred as string initially.
+// ------ALLOWED-------
+let simpleText = "hello";
+simpleText = "It's a simple text";
+let somethingHard = "Not a simple text";
+// ------NOT ALLOWED-------
+// somethingHard = true; // boolean type is not assignable to type string. Means k somethingHard variable expect kar raha tha k use string type ki value hi assign hoti but ham use boolean type ki value assign kar rahe hain.
+// Error main jo type left side par hai, declaration k waqt wo type right side par hogi. for example:
+// Type boolean is not assignable to type string.
+// somethingHard = true -> true (boolean) is on right side and (inferred type) string is on left side.
+// Type Annotation
+// any
+// Agar kisi variable ko initially value assign nahi ki ho tu Typescript future ki type expect karte hoe isko chor nahi degi bal k us variable ko 'any' type ka bana degi.
+// evolution of any type
+// Typescript main types evolve hoti rehti hain agar variable ko without initial value declare kiya ho.
+// For example, this is allowed:
+// Evolution of type
+let rocker; //In Typescript, this will be of 'any' type.
+rocker = "This is rocker";
+rocker.toLowerCase();
+rocker = 92;
+rocker.toPrecision(1);
+rocker.toUpperCase(); // Type error
+// Error 'toUpperCase() does not exist on type number. Because upper rocker.toPrecision(1) number ban chuka hai. (I mean rocker variable ki type evolve ho chuki hai)
+// Unnecessary Type anotation - because here the 'string' type annotation is redundant but the code doesn't throw any error.
+let txt = "Abdullah";
 /* // any type
 let obj: any = {x:0};
 
--- 00q1-quiz-prep/index.ts
+++ 00q1-quiz-prep/index.ts
@@ -59,9 +59,52 @@ function paintPainting(painter: Painter, painting: string) {
 // console.log(typeof(bestAudio)) // Inferred type: string
 
 // Type errors
-let fullname = "Abdullah"
-fullname.length() // Type error - Typescript be fullname variable ki type "string" infer ki thi and "length" property "number" hai and not function.
+// let fullname = "Abdullah"
+// fullname.length() // Type error - Typescript be fullname variable ki type "string" infer ki thi and "length" property "number" hai and not function.
 // fullname.length // No Type error
+
+
+// Assignability
+// These are allowed because we are assigning string to the variable which type was inferred as string initially.
+// ------ALLOWED-------
+let simpleText = "hello";
+simpleText = "It's a simple text";
+
+let somethingHard = "Not a simple text";
+// ------NOT ALLOWED-------
+// somethingHard = true; // boolean type is not assignable to type string. Means k somethingHard variable expect kar raha tha k use string type ki value hi assign hoti but ham use boolean type ki value assign kar rahe hain.
+// Error main jo type left side par hai, declaration k waqt wo type right side par hogi. for example:
+// Type boolean is not assignable to type string.
+// somethingHard = true -> true (boolean) is on right side and (inferred type) string is on left side.
+
+// Type Annotation
+// any
+// Agar kisi variable ko initially value assign nahi ki ho tu Typescript future ki type expect karte hoe isko chor nahi degi bal k us variable ko 'any' type ka bana degi.
+// evolution of any type
+// Typescript main types evolve hoti rehti hain agar variable ko without initial value declare kiya ho.
+// For example, this is allowed:
+
+// Evolution of type
+let rocker; //In Typescript, this will be of 'any' type.
+rocker = "This is rocker";
+rocker.toLowerCase();
+
+rocker = 92;
+rocker.toPrecision(1);
+
+// rocker = "hello"
+// rocker.toUpperCase(); // Type error and to resolve this type error, declare variable of type string.
+// Error 'toUpperCase() does not exist on type number. Because upper rocker.toPrecision(1) number ban chuka hai. (I mean rocker variable ki type evolve ho chuki hai)
+
+// Unnecessary Type anotation - because here the 'string' type annotation is redundant but the code doesn't throw any error.
+let txt: string = "Abdullah";
+
+
+
+
+let thisIsString : string = 23; // Type-error
+// 
+
 
 /* // any type
 let obj: any = {x:0};

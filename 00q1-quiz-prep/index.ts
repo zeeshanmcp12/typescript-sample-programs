5a18ab160d35954b61859f336c50ada5c92d3b4d
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-10-06 06:41:39 +0500

    End of chapter 2 from typescript book

--- 00q1-quiz-prep/index.ts
+++ 00q1-quiz-prep/index.ts
@@ -1,5 +1,7 @@
 // console.log("Hello World")
 
+// ============================================== Chapter 1 ==============================================
+
 /**
  * Chapter 1
 const fullName = "Abdullah"
@@ -42,26 +44,24 @@ function paintPainting(painter: Painter, painting: string) {
     console.log(typeof(painter.ownMaterials))
 } 
  */
+// ============================================== End of Chapter 1 ==============================================
+
+// ============================================== Chapter 2 ==============================================
 
 /**
  * Chapter 2
- * 
- * 
- */
 
-
-
-// Type Inferring
+Type Inferring
 // Typescript khud se hi type infer karleti hai because of being it smart enough. Typescript har us variable ki type infer kar sakti hai jiski starting value computed ho.
 // In this example, TypeScript knows that the ternary expression always results in a string
 
-// let bestAudio = Math.random() > 0.5 ? "Hamd" : "Simple Voice"
-// console.log(typeof(bestAudio)) // Inferred type: string
+let bestAudio = Math.random() > 0.5 ? "Hamd" : "Simple Voice"
+console.log(typeof(bestAudio)) // Inferred type: string
 
 // Type errors
-// let fullname = "Abdullah"
-// fullname.length() // Type error - Typescript be fullname variable ki type "string" infer ki thi and "length" property "number" hai and not function.
-// fullname.length // No Type error
+let fullname = "Abdullah"
+fullname.length() // Type error - Typescript be fullname variable ki type "string" infer ki thi and "length" property "number" hai and not function.
+fullname.length // No Type error
 
 
 // Assignability
@@ -93,17 +93,108 @@ rocker = 92;
 rocker.toPrecision(1);
 
 // rocker = "hello"
-// rocker.toUpperCase(); // Type error and to resolve this type error, declare variable of type string.
+rocker.toUpperCase(); // Type error and to resolve this type error, declare variable of type string as we declared in above line.
 // Error 'toUpperCase() does not exist on type number. Because upper rocker.toPrecision(1) number ban chuka hai. (I mean rocker variable ki type evolve ho chuki hai)
 
 // Unnecessary Type anotation - because here the 'string' type annotation is redundant but the code doesn't throw any error.
 let txt: string = "Abdullah";
 
-
-
-
 let thisIsString : string = 23; // Type-error
-// 
+// Here Typescript sees an incompatibility. Because assigned type to variable is "string" but initializer is "number".
+
+// Type Shapes
+
+// TypeScript understand karti hai k jis type ka variable hamne declare kiya hai uski konsi properties par konsa operation perform kiya ja sakta hai or konsa nahi.
+// For example,
+let raper = "Queen latifah";
+// Now, TypeScript inferred it's type as 'string'. Ab TypeScript ko malom hai is (string) type k variable ki konsi properties hain jis par string k operations perform kiye ja sakte hain. For example:
+raper.length // Allowed
+
+// Konse operation allowed nahi hain:
+raper.push("!") // Now Allowed because push is not a property of string
+
+// Same goes for objects. Object main hamne jo properties define ki hain agar un k ilawa ham kisi aisi property ko access karenge jo exist nahi karti to TypeScript error throw karegi. for example:
+
+interface Person {
+    name: string;
+    age: number
+}
+
+// This is called object's shape
+let father: Person = {
+    name: "Abdullah",
+    age: 30
+}
+
+father.name; // OK
+father.age; // OK
+father.middleName; // Error :Property 'middleName' does not exist on type 'Person'.ts(2339)
+
+// Module
+
+// JavaScript main pehle ye feature nahi tha k files (k content) ko apas main share kis tarah kiya jaye. ECMAScript 2015 main isko standardize kiya gaya "ECMAScript Module (ESM)" k name se.
+
+// Module - A file with a Top-level import or export 
+// Script - A file that is not a module.
+
+// Syntax
+
+import { value } from "./values"
+
+export const doubled = value * 2;
+
+
+// Same named variable multiple modules main use ho sakta hai and dono files main nameing conflict consider nahi hoga. for example
+
+// a.ts
+// export const shared = "Cher";
+
+// b.ts
+// export const shared = "Cher";
+
+// ================================== in above, there is no issue at all ==================================
+
+// c.ts
+// import { shared } from './a'
+// export const shared = "Cher";
+// ================================== type error =======================================
+// Because c.ts main hamne same name 'shared' k sath variable declare kiya hai, ab ham usi name k variable ko kisi dosri file se import bhi kar rahe hain tu ye naming conflict ho jayega between the imported "shared" with it's own (export const shared = "Cher") value.
+
+// Agar file script hai (and module nahi hai tu TypeScript use globally scoped consider karti hai and ) uske variables globally scoped honge. Means k agar kisi script main "human" name se variable declare kiya hai tu dosri script (file) usko access kar sakti hai and same (human) name se kisi dosri script file main declare nahi kiya ja sakta.
+
+// a.ts
+// const human = "Cher"
+// Cannot redeclare block-scope variable 'human'
+
+// b.ts
+// const human = "Cher"
+// Cannot redeclare block-scope variable 'human'
+
+// How to resolve above issue?
+// Hame export or import statements us file main add karni hongi.
+
+// How to enfore a file to be a module
+// file main kahin bhi export {} likh den tu wo file as a module work karegi.
+
+// a.ts and b.ts
+// const human = "Cher"; // OK
+// export {}
+
+ */
+// ============================================== End of Chapter 2 ==============================================
+
+// ============================================== Chapter 3 ==============================================
+
+/**
+ * Chapter 3
+ * 
+ */
+
+
+
+
+
+
 
 
 /* // any type
